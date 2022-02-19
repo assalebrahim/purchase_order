dd98e8f6549b9139d62fff76ecff07869bbb8499
assalebrahim <41667914+assalebrahim@users.noreply.github.com>
2022-02-19 19:40:20 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let _res_data = ""
+
+//get the data from dbs
+
+//selecting the list div 
+let _list_items = document.querySelectorAll("._list_items");
+
+//adding the data to div element
+_list_items.innerHTML = _res_data
+
+
+//get check box status
+
+
+//if check box is on then filter the result
+
+
+//get the data from add data
+
+
+//put new data to dbs
+
+
+
+
+
